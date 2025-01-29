22e36e5caf51be270830dea7c06c7b3b8473c00b
smpmaneth <smpmaneth@gmail.com>
2025-01-29 10:41:50 +0530

    update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -639,7 +639,7 @@
                             d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
                             clip-rule="evenodd"></path>
                     </svg>
-                    <span class="flex-1 ml-3 text-left whitespace-nowrap">Pages</span>
+                    <span class="flex-1 ml-3 text-left whitespace-nowrap">Users</span>
                     <svg
                         aria-hidden="true"
                         class="w-6 h-6"
@@ -656,17 +656,13 @@
                     <li>
                         <a
                             href="#"
-                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 test:text-white test:hover:bg-gray-700">Settings</a>
+                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-white rounded-lg transition duration-75 group hover:bg-gray-100 hover:text-gray-700">All Users</a>
                     </li>
+                    
                     <li>
                         <a
                             href="#"
-                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 test:text-white test:hover:bg-gray-700">Kanban</a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 test:text-white test:hover:bg-gray-700">Calendar</a>
+                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-white rounded-lg transition duration-75 group hover:bg-gray-100 hover:text-gray-700">Customers</a>
                     </li>
                 </ul>
             </li>
